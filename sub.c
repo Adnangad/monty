09f15b7778d0a2da6a3a5a9cab158af36549faae
Adnangad <gardobuyaadnan@gmail.com>
2023-12-14 10:12:51 -0800

    task-6

--- sub.c
+++ sub.c
@@ -16,7 +16,7 @@ void sub(stack_t **stack, unsigned int line_number)
 
         if (p == NULL || p->next == NULL)
         {
-                fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+                fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
                 exit(EXIT_FAILURE);
         }
         rez = p->next->n - p->n;
